0e32d0e87ab9f6383f0aa331733c312bb0bf77b8
diamondmaster1 <heerbachada@gmail.com>
2024-07-21 10:16:19 +0100

    added a new enemy type and created some conditions

--- adventure_game.py
+++ adventure_game.py
@@ -43,6 +43,11 @@ ice_giant_skill=7
 ice_giant_strength=12
 ice_giant_level=4
 
+grim_reaper_health=1000
+grim_reaper_skill=50
+grim_reaper_strength=120
+grim_reaper_level=30
+
 
 def hero_attack_goblin():
     global goblin_health
@@ -79,6 +84,24 @@ def hero_attack_ice_giant():
         time.sleep(0.5)
         print("too bad hero")
 
+def hero_attack_grim_reaper():
+    global grim_reaper_health
+    if dice()>hero_skill:
+        print("you have hit the 'grim reaper'")
+        time.sleep(0.5)
+        damage=dice()*hero_strength+hero_level
+        time.sleep(0.5)
+        print(damage,"damage")
+        time.sleep(0.5)
+        grim_reaper_health -= damage
+        time.sleep(0.5)
+        print("remaining 'grim reaper' health is",grim_reaper_health)
+        time.sleep(0.5)
+    else:
+        time.sleep(0.5)
+        print("too bad hero")
+
+
 
 
 def goblin_attack():
@@ -114,6 +137,24 @@ def ice_giant_attack():
     else:
         time.sleep(0.5)
         print("too bad ice giant;better luck next time")
+
+def grim_reaper_attack():
+    global hero_health
+    if dice()>grim_reaper_strength:
+        print("you have hit the hero;it is time for 'him' to die? ")
+        time.sleep(0.5)
+        damage=dice()*grim_reaper_strength+grim_reaper_level
+        time.sleep(0.5)
+        print(damage,"damage;you might actually die now")
+        time.sleep(0.5)
+        hero_health-= damage
+        time.sleep(0.5)
+        print("remaining hero health is",hero_health)
+    else:
+        time.sleep(0.5)
+        print("too bad ice giant;better luck next time")
+
+
 
 
 
@@ -170,6 +211,35 @@ def ice_giant_fight_hero():
                 break
             break
 
+def grim_reaper_fight_hero():
+    global hero_level
+    global current_exp
+    while hero_health > 0:
+        hero_attack_grim_reaper()
+        if grim_reaper_health > 0:
+            grim_reaper_health()
+            if hero_health < 1:
+                print("you are dead")
+                time.sleep(0.5)
+                print("you were destined to lose this fight as the 'grim reaper' is the most powerful foe in the 'magical cinematic universe'")
+                time.sleep(0.5)
+                break
+        else:
+            print("you win!!!!!!")
+            exp=random.randint(500,1000)
+            for i in range(exp):
+                current_exp += i
+                print("you have gained",current_exp)
+                if current_exp > 500:
+                    hero_level += 1
+                    print("you have just leveled up")
+                    time.sleep(0.5)
+                    print("your level is now",hero_level)
+                else:
+                    print("you have not leveled up yet")
+                break
+            break
+
 
 
 def goblin_fight_time():
@@ -183,6 +253,14 @@ def goblin_fight_time():
 def ice_giant_fight_time():
     start_time=time.time()
     ice_giant_fight_hero()
+    end_time=time.time()
+    fight_time=end_time-start_time
+    fight_time=round(fight_time,2)
+    print("your fight time is",fight_time,"s")
+
+def grim_reaper_fight_time():
+    start_time=time.time()
+    grim_reaper_fight_hero()
     end_time=time.time()
     fight_time=end_time-start_time
     fight_time=round(fight_time,2)
@@ -305,99 +383,130 @@ fast_text("it will be added to your inventory")
 time.sleep(0.5)
 inventory.append("missiles")
 print(inventory)
-item_shop()
-fast_text("There is a collosal stone wall in front of you")
+fast_text("you suddenly see a giantic figure in the distance")
 time.sleep(0.5)
-fast_text("You shoot it down with your missiles")
+fast_text("you are naturally hesitate to approach")
 time.sleep(0.5)
-fast_text("you walk down a narrow, spooky road until you reach a crossroad that will determine your fate")
+fast_text("However, the giantic figure starts to approach to a deathly walts")
 time.sleep(0.5)
-choice_1=input("you can choose between the icy depths or the firey hellscape")
-if choice_1 == "icy depths":
-    fast_text("There is a sign saying welcome and beware of any icy giants")
+fast_text("You start to panic as you start to equipe your missiles")
+time.sleep(0.5)
+fast_text("As the figure walts closer and closer to you, you find out he is the 'grime reaper'")
+time.sleep(0.5)
+fast_text("'you have to fight it'you quickly start to realise")
+time.sleep(0.5)
+fast_text("it's time to fight hero")
+time.sleep(0.5)
+fast_text("the 'grim reaper' suddenly becomes more friendly as you have failured to kill him")
+time.sleep(0.5)
+fast_text("But, he is willing to make a deal")
+time.sleep(0.5)
+fast_text("You can either use his 'item shop' and gain the benefits but if you accept,there will be 3 rules in the contract")
+time.sleep(0.5)
+fast_text("Rule 1: you can't harm or kill the 'grim reaper' ever")
+time.sleep(0.5)
+fast_text("Rule 2: You can never ever kill wild animals or innocent civilans")
+time.sleep(0.5)
+fast_text("Rule 3: You can never harm 'the king' or the 'big bad' in this universe (this is the reason why you set out on this journey in the first place btw)")
+time.sleep(0.5)
+fast_text("If you don't accept, you can carry on your journey but there will be no hope in killing the 'big bad'")
+time.sleep(0.5)
+chocie_0=fast_text("Do you accept this conditions? (y/n)")
+if chocie_0 == "y":
+    fast_text("congratulations, you are able to use the item shop, happy shopin       for now")
+    item_shop()
+    fast_text("There is a collosal stone wall in front of you")
     time.sleep(0.5)
-    fast_text("You start to feeling empty inside and start craving a warm coat")
+    fast_text("You shoot it down with your missiles")
     time.sleep(0.5)
-    fast_text("You start to lose health by the minute")
+    fast_text("you walk down a narrow, spooky road until you reach a crossroad that will determine your fate")
     time.sleep(0.5)
-    fast_text("You suddenly see a snoty nosed goblin with a thick, warm coat on it")
-    time.sleep(0.5)
-    fast_text("You decided to fight the being in combat;it's time to fight hero")
-    time.sleep(0.5)
-    goblin_fight_time()
-    time.sleep(0.5)
-    fast_text("you have accquired the warm coat")
-    time.sleep(0.5)
-    fast_text("It will be added into your inventory")
-    time.sleep(0.5)
-    inventory.append("warm coat")
-    print(inventory)
-    time.sleep(0.5)
-    fast_text("you start to feel as if your heart is flooding in with emotions again")
-    time.sleep(0.5)
-    fast_text("You start to dance as if you were a child again")
-    time.sleep(0.5)
-    fast_text("But your mind rudely interupts you with your task to rescue your sister from the 'meancing monster' ")
-    time.sleep(0.5)
-    fast_text("You give a shrug as in you are accepting your fate and decide to carry on the fight")
-    time.sleep(0.5)
-    fast_text("You decide the head into the Caves of Despair")
-    time.sleep(0.5)
-    fast_text("'But wait' your brain warns you about the dangers of the cave as you don't have a flashlight")
-    time.sleep(0.5)
-    choice_2=input("do you decide to 'walk as if you were blind' or 'accquire a flashlight in order to see'? ")
-    if choice_2 == "walk as if you were blind":
-        fast_text("you decide to pick the dumb option but a brave one in order to become 'the' hero")
+    choice_1=input("you can choose between the icy depths or the firey hellscape")
+    if choice_1 == "icy depths":
+        fast_text("There is a sign saying welcome and beware of any icy giants")
         time.sleep(0.5)
-        fast_text("you start to stumble")
+        fast_text("You start to feeling empty inside and start craving a warm coat")
         time.sleep(0.5)
-        fast_text("and suddenly a goblin appears from out of nowhere;it is time to fight hero")
+        fast_text("You start to lose health by the minute")
+        time.sleep(0.5)
+        fast_text("You suddenly see a snoty nosed goblin with a thick, warm coat on it")
+        time.sleep(0.5)
+        fast_text("You decided to fight the being in combat;it's time to fight hero")
         time.sleep(0.5)
         goblin_fight_time()
         time.sleep(0.5)
-        fast_text("you have just received malaria")
+        fast_text("you have accquired the warm coat")
         time.sleep(0.5)
-        fast_text("your health has decreased by 30")
+        fast_text("It will be added into your inventory")
         time.sleep(0.5)
-        hero_health -= 30
-        print("your health is now", hero_health)
+        inventory.append("warm coat")
+        print(inventory)
         time.sleep(0.5)
-        fast_text("you start to hop on one knee as if you are about to die")
+        fast_text("you start to feel as if your heart is flooding in with emotions again")
         time.sleep(0.5)
-        fast_text("But suddenly you see that an icy giant has the 'cure' for malaria")
+        fast_text("You start to dance as if you were a child again")
         time.sleep(0.5)
-        fast_text("Without thinking, you start to lunge towards the ice giant in order to fight for your life")
+        fast_text("But your mind rudely interupts you with your task to rescue your sister from the 'meancing monster' ")
         time.sleep(0.5)
-        fast_text("it's time to fight hero")
+        fast_text("You give a shrug as in you are accepting your fate and decide to carry on the fight")
         time.sleep(0.5)
-        ice_giant_fight_time()
+        fast_text("You decide the head into the Caves of Despair")
         time.sleep(0.5)
-        fast_text("you have received the medicine")
+        fast_text("'But wait' your brain warns you about the dangers of the cave as you don't have a flashlight")
         time.sleep(0.5)
-        fast_text("congratulations, you have just received 40 health")
-        time.sleep(0.5)
-        hero_health += 40
-        print("your health is now", hero_health)
-        time.sleep(0.5)
-        fast_text("you start to walk for an agonizing amount of minutes")
-        time.sleep(0.5)
-        fast_text("you start to see a lingering light in front of you") 
-        time.sleep(0.5)
-        fast_text("you want to walk towards it")
-        time.sleep(0.5)
-        fast_text("you have discovered the kitchen;there is an ice giant waiting for you")
-        time.sleep(0.5)
-        fast_text("It is time to fight")
-        time.sleep(0.5)
-        ice_giant_fight_time()
-        time.sleep(0.5)
-        fast_text("you have reached 'the outside world'")
-        time.sleep(0.5)
-        fast_text("you suddenly see a strange figure in the distance")
-        time.sleep(0.5)
-        fast_text("It is the stranger you met eariler in your journey")
-        time.sleep(0.5)
-        fast_text("It is time to shop")
-        time.sleep(0.5)
-        item_shop()
-        time.sleep(0.5)
+        choice_2=input("do you decide to 'walk as if you were blind' or 'accquire a flashlight in order to see'? ")
+        if choice_2 == "walk as if you were blind":
+            fast_text("you decide to pick the dumb option but a brave one in order to become 'the' hero")
+            time.sleep(0.5)
+            fast_text("you start to stumble")
+            time.sleep(0.5)
+            fast_text("and suddenly a goblin appears from out of nowhere;it is time to fight hero")
+            time.sleep(0.5)
+            goblin_fight_time()
+            time.sleep(0.5)
+            fast_text("you have just received malaria")
+            time.sleep(0.5)
+            fast_text("your health has decreased by 30")
+            time.sleep(0.5)
+            hero_health -= 30
+            print("your health is now", hero_health)
+            time.sleep(0.5)
+            fast_text("you start to hop on one knee as if you are about to die")
+            time.sleep(0.5)
+            fast_text("But suddenly you see that an icy giant has the 'cure' for malaria")
+            time.sleep(0.5)
+            fast_text("Without thinking, you start to lunge towards the ice giant in order to fight for your life")
+            time.sleep(0.5)
+            fast_text("it's time to fight hero")
+            time.sleep(0.5)
+            ice_giant_fight_time()
+            time.sleep(0.5)
+            fast_text("you have received the medicine")
+            time.sleep(0.5)
+            fast_text("congratulations, you have just received 40 health")
+            time.sleep(0.5)
+            hero_health += 40
+            print("your health is now", hero_health)
+            time.sleep(0.5)
+            fast_text("you start to walk for an agonizing amount of minutes")
+            time.sleep(0.5)
+            fast_text("you start to see a lingering light in front of you") 
+            time.sleep(0.5)
+            fast_text("you want to walk towards it")
+            time.sleep(0.5)
+            fast_text("you have discovered the kitchen;there is an ice giant waiting for you")
+            time.sleep(0.5)
+            fast_text("It is time to fight")
+            time.sleep(0.5)
+            ice_giant_fight_time()
+            time.sleep(0.5)
+            fast_text("you have reached 'the outside world'")
+            time.sleep(0.5)
+            fast_text("you suddenly see a strange figure in the distance")
+            time.sleep(0.5)
+            fast_text("It is the stranger you met eariler in your journey")
+            time.sleep(0.5)
+            fast_text("It is time to shop")
+            time.sleep(0.5)
+            item_shop()
+            time.sleep(0.5)
